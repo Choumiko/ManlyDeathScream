4832b763c0f0fd10abaae85b596d8d578502d353
Choumiko <ghostface@ghostface.info>
2017-12-13 16:05:47 +0100

    fixes for luacheck

--- control.lua
+++ control.lua
@@ -1,16 +1,16 @@
 local function play_sound(event)
     local _, _ = pcall(function()
-		if not game.active_mods["Pitch_Black"] then
-			local player = game.players[event.player_index]
-			player.surface.play_sound{path="manly_death_scream", position=player.position}
-			--player.surface.create_entity({name = "manly_death_scream", position=player.position})
-		end
-	end)
+        if not game.active_mods["Pitch_Black"] then
+            local player = game.players[event.player_index]
+            player.surface.play_sound{path="manly_death_scream", position=player.position}
+            --player.surface.create_entity({name = "manly_death_scream", position=player.position})
+        end
+    end)
 end
 
 script.on_init(function()
-    local _, err = pcall(function() 
-            script.on_event(defines.events.on_pre_player_died, play_sound)
+    local _, err = pcall(function()
+        script.on_event(defines.events.on_pre_player_died, play_sound)
     end)
     if err then
         log("Error occured")
--- data.lua
+++ data.lua
@@ -1,9 +1,8 @@
-local empty_image = "__ManlyDeathScream__/trans1.png"
 data:extend({
     {
         type = "sound",
         name = "manly_death_scream",
-		filename = '__ManlyDeathScream__/scream.ogg',
-		volume = 1
+        filename = '__ManlyDeathScream__/scream.ogg',
+        volume = 1
     }
 })
